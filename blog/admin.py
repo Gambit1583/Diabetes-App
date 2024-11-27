f932889d723c4a409d3de33275996c48a1d8fa4e
Paul Jebb <pjebb15@gmail.com>
2024-11-27 11:56:59 +0000

    Removed author model to allow admin user to populate author dropdown.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
+
+class PostAdmin(admin.ModelAdmin): 
+    ordering = ['-created_on']
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
+# admin.site.register(Author)
--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Models
 
-# Allows for author dropdown when creating/editing post in the admin panel.
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+# # Allows for author dropdown when creating/editing post in the admin panel.
+# class Author(models.Model):
+#     name = models.CharField(max_length=100)
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     featured_image = models.ImageField(upload_to='images/', null=True, blank=True)
     excerpt = models.TextField()
