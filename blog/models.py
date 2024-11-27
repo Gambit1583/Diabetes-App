cd1b8b64596ba53ba73dfebc9e367bb39beea8b5
Paul Jebb <pjebb15@gmail.com>
2024-11-27 10:51:56 +0000

    Added views and urls to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    author = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    featured_image = models.ImageField(upload_to='images/', null=True, blank=True)
+    excerpt = models.TextField()
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
