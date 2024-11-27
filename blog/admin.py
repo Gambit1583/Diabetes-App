d86c9cf44ad783e9b3fb77b1b6e92c0fd1b92062
Paul Jebb <pjebb15@gmail.com>
2024-11-27 16:03:28 +0000

    Fix image display issue and update admin configuration

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Post
 
 
-class PostAdmin(admin.ModelAdmin): 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_on', 'featured_image') 
+    prepopulated_fields = {'slug': ('title',)} 
     ordering = ['-created_on']
 
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
-# admin.site.register(Author)
