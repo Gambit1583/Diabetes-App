2f63e2ac6a37aff274436732c06a9c01db007ed0
Paul Jebb <pjebb15@gmail.com>
2024-11-27 11:33:50 +0000

    Added Author model and updated Post model with Meta class for ordering

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Models
+
+# Allows for author dropdown when creating/editing post in the admin panel.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     featured_image = models.ImageField(upload_to='images/', null=True, blank=True)
     excerpt = models.TextField()
     content = models.TextField()
 
+# Orders posts from newest to oldest.
+    class Meta:
+        ordering = ["-created_on"]
+
     def __str__(self):
-        return self.title
+        return f"The title of this post is {self.title}"
