5078afd75f7a5ff4b4fbedb8a94fdebf84c08409
Paul Jebb <pjebb15@gmail.com>
2024-11-27 15:16:46 +0000

    Made and applied migrations for updated Post model

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('post/<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def home_page(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', {'post_list': post_list})
+
+def blog_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/blog_detail.html', {'post': post})           
