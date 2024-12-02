8e718d2fe146a819840b746dd1b5b3a46dfe51ab
Paul Jebb <pjebb15@gmail.com>
2024-12-02 11:32:09 +0000

    TEST5 cleared the featured_image field on the 3 existing posts

--- blog/management/commands/update_featured_images.py
+++ blog/management/commands/update_featured_images.py
@@ -1,13 +1,13 @@
-from django.core.management.base import BaseCommand
-from blog.models import Post
+# from django.core.management.base import BaseCommand
+# from blog.models import Post
 
-class Command(BaseCommand):
-    help = 'Set default image for posts without a featured image'
+# class Command(BaseCommand):
+#     help = 'Set default image for posts without a featured image'
 
-    def handle(self, *args, **kwargs):
-        posts = Post.objects.all()
-        for post in posts:
-            if not post.featured_image:
-                post.featured_image = 'images/default.jpg'
-                post.save()
-        self.stdout.write(self.style.SUCCESS('Successfully updated posts with default images'))
+#     def handle(self, *args, **kwargs):
+#         posts = Post.objects.all()
+#         for post in posts:
+#             if not post.featured_image:
+#                 post.featured_image = 'images/default.jpg'
+#                 post.save()
+#         self.stdout.write(self.style.SUCCESS('Successfully updated posts with default images'))
