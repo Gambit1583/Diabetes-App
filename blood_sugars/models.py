e54e8da9d5b8a8577b9d7febe6c86fcf41acc184
Paul Jebb <pjebb15@gmail.com>
2024-12-03 11:38:39 +0000

    Added models for FoodDiaryEntry and DailyDiaryEntry

--- blood_sugars/models.py
+++ blood_sugars/models.py
@@ -5,7 +5,24 @@ class BloodSugarRecord(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     blood_sugar_level = models.FloatField()
-    carbohyfrates_eaten = models.FloatField()
+    carbohydrates_eaten = models.FloatField()
 
     def __str__(self):
         return f"Record for {self.user.username} on {self.date}"
+
+class FoodDiaryEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meal_time = models.DateTimeField
+    food_item = models.CharField(max_length=200)
+    calories = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.food_item} at {self.meal_time}"
+
+class DailyDiaryEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    entry_time = models.DateTimeField(auto_now_add=True)
+    content =models.TextField()
+
+    def __str__(self):
+        return f"Entry for {self.user.username} on {self.entry_time}"
